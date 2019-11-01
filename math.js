1a30aa018fc8f9e1bb48a3c7b258e9e9f1334661
StefanPowell <Stefan.Powell001@umb.edu>
2019-11-01 16:05:39 -0400

    update math

--- math.js
+++ math.js
@@ -13,15 +13,52 @@ function median(values){
 	//find the medain
 }
 
+function summ(func, start, end){
+	//need to parse the functions
+	sum = 0;
+	for(var z = start; z < end; z++){
+		sum = sum + values[z];
+	}
+	return sum;
+}
+
+//fix mode
 function mode(values){
-	//create dictionary
-	//increment values in dictionary
-	//add new values to dictionary if not already there
+	array_number = [];
+	array_count = [];
+	for(var x = 0; x < values.length; x++){
+		if(array_number.includes(values[x])){
+			array_count[x] = array_count[x] + 1;
+		}
+		else{
+			array_number[x] = values[x];
+			array_count[x] = 1;
+		}
+	}
+	return array_count;
 }
 
 function ascending(values){
 	for(var j=0; j < values.length; j++){
-		
+		//	
 	}
+}
+
+function descending(values){
+	//
+}
+
+
+function std(values){
+	//standard deviation
+}
+
+
+function vari(values){
+	//variance
+}
+
+function parse(func){
+	//
 }
 
