92e71bc7b3cd3dea6a70e1f09ee30d69a0f52b08
StefanPowell <Stefan.Powell001@umb.edu>
2019-11-01 12:48:02 -0400

    update

---
+++ graph.js
@@ -0,0 +1,13 @@
+//graph .js
+
+function bar_graph(x_values, y_values, xname, yname){
+	
+}
+
+function pie_chart(){
+	//
+}
+
+function histogram(){
+	//
+}
--- math.js
+++ math.js
@@ -5,3 +5,23 @@ function mean(values){
 	}
 	return (total/values.length);
 }
+
+function median(values){
+	//check if they are ordered properly
+		//if not ordered properly
+			//oreder properly
+	//find the medain
+}
+
+function mode(values){
+	//create dictionary
+	//increment values in dictionary
+	//add new values to dictionary if not already there
+}
+
+function ascending(values){
+	for(var j=0; j < values.length; j++){
+		
+	}
+}
+
