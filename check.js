9d6ef3a3574569507fa90e27fb529a1591d15db3
StefanPowell <Stefan.Powell001@umb.edu>
2020-01-22 02:31:13 -0500

    update graph.js

--- check.js
+++ check.js
@@ -1,3 +1,6 @@
+
+var total_expected = 45;
+
 function get_block_names(x){
   block_names = []
   while(block_names.length < 9){
@@ -31,7 +34,6 @@ function check_all_numbers(x){
     numbers = get_block_values(x);
     number_check = false;
     total_sum = 0;
-    total_expected = 45;
     for(h = 0; h < numbers.length; h++){
       total_sum = total_sum + parseInt(numbers[h]);
     }
@@ -43,14 +45,50 @@ function check_all_numbers(x){
   return number_check;
 }
 
-function horizontal_check(){
-  console.log();
+function check_row(array_cells){
+  total = 0
+  for(n=0;n<array_cells.length;n++){
+    total = total + parseInt(document.getElementById(array_cells[n]).value);
+    document.getElementById(array_cells[n]).style.borderColor = "red";
+  }
+  if(total == total_expected){
+    return true
+  }
+  else{
+    return false
+  }
 }
+
+
+function horizontal_check(){
+  get_rows_array = []
+  check_values =   []
+  rows = [1, 4, 7, 2, 5, 8, 3, 6, 9]
+  i = 1
+  incr = 3
+  while(i<2){
+    end = i*incr
+     for(c=1;c<4;c++){	                  //last digit
+      for(b=(end-incr); b<end; b++){ 	    //first digit
+        for(a=1; a<4; a++){			          //second digit
+  		      value = "cell" + rows[b].toString() + "_" + a.toString() + "_" + c.toString()
+  		      get_rows_array.push(value)
+  	    }
+      }
+      console.log(get_rows_array);
+      checked = check_row(get_rows_array);
+      check_values.push(checked)
+      get_rows_array = []
+      i = i + 1
+    }
+ }
+ console.log(check_values);
+}
+
 
 function vertical_check(){
   console.log();
 }
-
 //loop through all block_names 1-9
 //make sure that all blocks are full - can put in check_all_numbers
 //make sure that all blocks values combined equals 45
@@ -58,10 +96,23 @@ function vertical_check(){
 //do vertical check
 //make sure
 
+function show_message(){
+  document.getElementById("message_box").innerHTML = "Empty Cells Found";
+}
+
 function full_check(){
+  
   isfull = [];
-  for(x=1;x<4;x++){
+  keep_track = true;
+
+  for(x=1;x<10;x++){
     isfull.push(check_all_numbers(x));
   }
-  console.log(isfull);
+  for(i=0; i < 9; i++)
+  {if(isfull[i] == false){show_message();keep_track = false;}}
+
+  if(keep_track == true){
+    console.log("all populated");
+  }
+
 }
--- graph.js
+++ graph.js
@@ -9,7 +9,12 @@ function getmax(values){
 	return max;
 }
 
+function resetgraph(){
+	document.getElementById('hold').innerHTML = "";
+}
+
 function plotbargraph(barscount, y_values) {
+	//takes the number of bars and the array of their values
   var bar_width = 100/barscount;
   var max_height = getmax(y_values);
   const div = document.createElement('div');
@@ -22,13 +27,35 @@ function plotbargraph(barscount, y_values) {
   	str = str.concat(div_add);
   }
   div.innerHTML = str;
-  document.getElementById('hold').appendChild(div);  
+  document.getElementById('hold').appendChild(div);
 }
 
 function bar_graph(x_values, y_values){
 	var xcount = x_values.length;
 	var ycount = y_values.length;
 	plotbargraph(xcount, y_values);
+}
+
+function scatterplot(xvalues, yvalues){
+	//takes an array of x and y values
+	document.getElementById('hold').style = "border:1px solid red;";
+	xvalues_max = getmax(xvalues);
+	yvalues_max = getmax(yvalues);
+	var points = "";
+	for(var z = 0; z < yvalues.length; z++){
+		pointx = 100 - ((xvalues[z]/xvalues_max)*100);
+		pointy = (yvalues[z]/yvalues_max)*100;
+		var newpoint = "<span style='position:fixed;top:"+pointy+"%;left:"+pointx+"%;' class='dot'></span>";
+		points = points.concat(newpoint);
+	}
+	document.getElementById('hold').innerHTML = points;
+}
+
+function linegraph(){
+	//use scatterplot but with restrictions and add lines
+	document.getElementById('hold').style = "border:1px solid red;";
+	document.getElementById("lineg").style.display = "block";
+	document.getElementById("poly").setAttribute('points', "0,0 0,100" );
 }
 
 function pie_chart(){
@@ -38,7 +65,3 @@ function pie_chart(){
 function histogram(){
 	//
 }
-
-
-
-
